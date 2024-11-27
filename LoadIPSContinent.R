1bbebe6f446dadacfc3899f50a9d58ea402d2173
eb1383 <47615530+eb1383@users.noreply.github.com>
2024-11-27 12:00:12 +0000

    LoadIPSContinent.R

--- LoadIPSContinent.R
+++ LoadIPSContinent.R
@@ -1,4 +1,3 @@
-
 # libraries
 library(tidyverse)
 
@@ -11,51 +10,26 @@ df <- df %>%
   summarize(Visits = sum(Weight)) %>% 
   rename(Destination = Country)
 
-
-
-
-
-
 # load continent data 
 continents <- read_csv("CountriesContinents.csv")
-
 colnames(continents) <- c("Continent", "Destination")
-
 # join continents to ips 
 df <- left_join(df, continents, by = "Destination")
-
-
-
-
-
 # replace destination with continent 
 df$Destination <- ifelse(df$Destination %in% destinations, df$Destination, df$Continent)
-
 # compute the new total number of visits for each destination and month
 df <- df %>%
   group_by(Year, Month, Destination) %>%
   summarize(Visits = sum(Visits)) 
 
 
-
-
-
 # load population data
 pop <- read_csv("Population_16_19.csv")
-
 df <- left_join(df, pop, by = "Year")
-
-
-
-
-
-
-
 # compute the total number of visits for each month
 df_visits <- df %>%
   group_by(Year, Month) %>%
   summarize(Total_Visits = sum(Visits))
-
 # compute the number of people who stayed at home for each month
 df_domestic <- df_visits %>%
   left_join(pop, by = "Year") %>%
@@ -65,51 +39,23 @@ df_domestic <- df_visits %>%
 
 df <- bind_rows(df, df_domestic)
 
-
-
-
-
-
-
-
-
-
-
-
 # set up array to store data
 n <- array(0, c(dest + 1, timepoints))
 
-
-
-
-
 for(t in 1 : timepoints){
-  
   for (i in 1 : (dest + 1)){
-    
-    if(i <= dest){
-      
+    if(i <= dest){ 
       wh <- which(df$Month == getMonth(t) & df$Year == years[getYear(t)] & df$Destination == destinations[i])
     }
-    
-    
     else {
-      
       wh <- which(df$Month == getMonth(t) & df$Year == years[getYear(t)] & df$Destination == "Domestic")
-    }
-    
-    if(length(wh) == 1){
-      
-      n[i,t] <- df$Visits[wh]
-      
-    }
-    
+    }    
+    if(length(wh) == 1){      
+      n[i,t] <- df$Visits[wh]     
+    }    
     else if(length(wh) > 1){
-      
       print('stop')
     }
-    
-    
   }
   
 }
